a51d1e3c87d3a321d92f5f5037af2e59c7e02f6c
josnnydev <pjosnny@gmail.com>
2025-04-02 20:34:43 -0400

    add superuser

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --username josnny --email josnny@gmail.com --password 8092388674jpv
+
 
 
 
